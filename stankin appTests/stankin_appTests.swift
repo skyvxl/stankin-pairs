9747ba85b321094e91887a97b6137fb43605dc4a
skyvxl <wivvz@yandex.ru>
2026-03-01 17:01:32 +0300

    Initial Commit

---
+++ stankin appTests/stankin_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  stankin_appTests.swift
+//  stankin appTests
+//
+//  Created by Дмитрий Нилов on 01.03.2026.
+//
+
+import Testing
+@testable import stankin_app
+
+struct stankin_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
