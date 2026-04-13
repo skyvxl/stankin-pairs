5fd3ea0a39668d52cd1ad698f04664b8db6ce848
skyvxl <wivvz@yandex.ru>
2026-04-13 20:06:23 +0300

    feat: add @vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-  import { injectSpeedInsights } from '@vercel/speed-insights';
   import '../app.css';
+	import { dev } from '$app/environment';
+  import { injectSpeedInsights } from '@vercel/speed-insights';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
   let { children } = $props();
+  
   injectSpeedInsights();
+  injectAnalytics({ mode: dev? 'development' : 'production' });
 </script>
 
 {@render children?.()}
